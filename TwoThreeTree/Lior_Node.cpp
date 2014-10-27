af1b761b7cb954be13259c714cf4998482739f79
Lior Asulin <Lior035@gmail.com>
2014-10-27 14:15:21 +0200

    Add files

---
+++ TwoThreeTree/Lior_Node.cpp
@@ -0,0 +1,18 @@
+#include "Lior_Node.h"
+
+int Lior_Node::IsLeaf()
+{
+	return isleaf;
+}
+
+void Lior_Node::SetParent(Lior_Node* p)		
+{
+	parent = p;
+}
+	
+Lior_Node* Lior_Node::getParent()		
+{
+	return parent;
+}
+
+
